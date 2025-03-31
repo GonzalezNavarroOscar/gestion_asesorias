3745a7e2912d311972ebf5d28057c43589adb945
González Navarro Oscar Eduardo <158109661+GonzalezNavarroOscar@users.noreply.github.com>
2025-03-31 00:58:29 -0700

    cambios en server.js para metodos get de login

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path'); 
+
 const app = express();
 const port = 3000;
 
 const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
-
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 app.use('/api', authRoutes);
 
