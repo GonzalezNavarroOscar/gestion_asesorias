ba462797550e664eb1600a53e8079f28bb487cb5
González Navarro Oscar Eduardo <158109661+GonzalezNavarroOscar@users.noreply.github.com>
2025-04-21 04:50:30 -0700

    Certificados autofirmados para envío de correo

--- backend/mail.js
+++ backend/mail.js
@@ -19,6 +19,9 @@ class Email {
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
     }
