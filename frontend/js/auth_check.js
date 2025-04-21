db04f5fbc5cb6a264744e8bb35b89c1d392161e6
González Navarro Oscar Eduardo <158109661+GonzalezNavarroOscar@users.noreply.github.com>
2025-04-21 15:18:45 -0700

    Modificación en autenticador de tokens, inclusión de autenticador en todos los archivos HTML

--- frontend/js/auth_check.js
+++ frontend/js/auth_check.js
@@ -2,5 +2,6 @@ const token = localStorage.getItem('authToken');
 const user = localStorage.getItem('userData');
 
 if (!token || !user) {
+  alert('Debes iniciar sesión!');
   window.location.href = '../index.html';
 }
